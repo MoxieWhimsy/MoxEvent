38897e4fa1339be87ee943009dde63e5400426be
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2021-08-16 19:04:03 -0400

    Add Vector Two Event

---
+++ #Code/VectorTwoEvent.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Mox.Events
+{
+	[CreateAssetMenu(menuName = "Event/Vector2", fileName = "new VectorTwo Event", order = 0)]
+	public class VectorTwoEvent : AGlobalEvent<Vector2>
+	{
+	}
+}
