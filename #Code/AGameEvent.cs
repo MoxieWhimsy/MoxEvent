01ea4532217d436e62ec5a56bfc39a357dc11cfc
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2022-07-11 19:48:49 -0400

    feat: make events' `Register` and `Unregister` methods public

--- #Code/AGameEvent.cs
+++ #Code/AGameEvent.cs
@@ -6,8 +6,8 @@ namespace Mox.Events
 	{
 		public abstract void Raise(T item);
 
-		internal abstract void Register(IGameEventListener<T> listener);
+		public abstract void Register(IGameEventListener<T> listener);
 
-		internal abstract void Unregister(IGameEventListener<T> listener);
+		public abstract void Unregister(IGameEventListener<T> listener);
 	}
 }
