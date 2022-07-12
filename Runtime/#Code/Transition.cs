f009e042ed5ac4e18b12fbb69df77037d7961d7f
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2022-07-12 18:15:57 -0400

    fix: can compile Transition without Odin Inspector
    Though formatting will be better with Odin Inspector

--- Runtime/#Code/Transition.cs
+++ Runtime/#Code/Transition.cs
@@ -1,13 +1,19 @@
-﻿using Sirenix.OdinInspector;
+﻿#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 
 namespace Mox.Events
 {
 	[System.Serializable]
 	public class Transition<T>
 	{
+		#if ODIN_INSPECTOR
 		[HorizontalGroup]
+		#endif
 		public AGameEvent<T> trigger;
+		#if ODIN_INSPECTOR
 		[HorizontalGroup]
+		#endif
 		public AGameState<T> target;
 	}
 }
